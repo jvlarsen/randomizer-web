ccce2ca01adfbe34677f224932d02cdc1f9551c5
Jesper Vehn Larsen <pilotlarsen@gmail.com>
2017-07-18 20:42:56 +0200

    Pointing to use Connector instead of in-component function.

--- randomizer-client/src/helpers/Connector.js
+++ randomizer-client/src/helpers/Connector.js
@@ -1,2 +1,13 @@
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:55226/';
+
+const countClicks = () => {
+	axios.get(`${apiUrl}/api/participants`);
+	console.log('Function called.');
+}
 
 
+export default {
+	countClicks,
+}
